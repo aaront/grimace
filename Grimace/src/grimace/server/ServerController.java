ca710774810c66a5df3243484da29e2c9aa01139
vineet.sharma10 <vineet.sharma10@708ad52c-d002-11de-97bf-6350044336de>
2009-11-29 16:23:34 +0000

    Added class grimace.server.ServerController.

---
+++ Grimace/src/grimace/server/ServerController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grimace.server;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Vineet Sharma
+ */
+public class ServerController {
+    private static final int LISTENING_PORT = 1234;
+	private static ServerSocket[] connections;
+
+	public static void setupServer() {
+
+	}
+
+	public static void listen() {
+
+	}
+
+	public static void initServerSocket() {
+
+	}
+
+	public static void setupConnection() {
+
+	}
+
+	public static void decodeCommand(Command cmd) {
+
+	}
+
+	public static void checkAccountLoginStatus(String username) {
+
+	}
+
+	public static void verifyLoginRequest(String username, String password) {
+
+	}
+
+	public static void placeContactRequest(String username, String contactname) {
+
+	}
+
+	public static void confirmContactRequest(String username, String contactName) {
+
+	}
+}
