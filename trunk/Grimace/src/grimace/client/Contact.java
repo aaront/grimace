d00f9a7eeec44760c52adbadce0d058a13270977
atoth89 <atoth89@708ad52c-d002-11de-97bf-6350044336de>
2009-11-29 20:16:00 +0000

    Fixed licensing. Use this template for licensing, replacing the filename and author to your filename and name.

--- trunk/Grimace/src/grimace/client/Contact.java
+++ trunk/Grimace/src/grimace/client/Contact.java
@@ -1,26 +1,29 @@
-// Wernicke: A chat client in Java.
-// Copyright (C) 2009 Justin Cole, Aaron Jankun, David Marczak Vineet Sharma,
-//         and Aaron Toth
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Contact.java
+ *
+ * @author Aaron Toth
+ *
+ * Copyright (C) 2009 Justin Cole, Aaron Jankun, David Marczak Vineet Sharma,
+ *        and Aaron Toth
+ *
+ * This file is part of Wernicke.
+ *
+ * Wernicke is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package grimace.client;
 
-/**
- *
- * @author aaront
- */
 public class Contact {
 
     // @TODO: Interact with the database somehow. I think. Maybe not.
