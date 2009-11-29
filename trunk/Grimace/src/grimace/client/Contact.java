e6637744e4d04e5ead25741ea7ae3f5093d8d0b3
atoth89 <atoth89@708ad52c-d002-11de-97bf-6350044336de>
2009-11-29 19:37:16 +0000

    Added frameworks of my code responsibilities. Logic to come soon...

---
+++ trunk/Grimace/src/grimace/client/Contact.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grimace.client;
+
+/**
+ *
+ * @author aaront
+ */
+public class Contact {
+
+    // @TODO: Interact with the database somehow. I think. Maybe not.
+
+    private static String userName;
+    private static String displayName;
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getDisplayName() {
+        return displayName;
+    }
+
+}
---
+++ trunk/Grimace/src/grimace/client/ContactList.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grimace.client;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aaron Toth
+ */
+
+public class ContactList {
+
+    // Assuming that we're using an array data structure
+    private static ArrayList<Contact> list = new ArrayList();
+    
+    public static void addContact(Contact toAdd) {
+        
+    }
+    
+    public static void removeContact(Contact toRemove) {
+        
+    }
+    
+    public static ArrayList<Contact> getList() {
+        return list;
+    }
+
+}
