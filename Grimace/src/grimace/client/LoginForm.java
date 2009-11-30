d7dbfa9b34a4d38caad7cbd37ea4d3831913254f
jjcole <jjcole@708ad52c-d002-11de-97bf-6350044336de>
2009-11-30 00:13:05 +0000

    Pre-dinner commit of partial work on forms and ProgramSettings.

--- Grimace/src/grimace/client/LoginForm.java
+++ Grimace/src/grimace/client/LoginForm.java
@@ -11,6 +11,8 @@
 
 package grimace.client;
 
+import grimace.server.Command;
+
 /**
  *
  * @author Huxley
@@ -38,6 +40,8 @@ public class LoginForm extends javax.swing.JPanel {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         jTextField1.setText("Wernicke");
 
@@ -52,27 +56,43 @@ public class LoginForm extends javax.swing.JPanel {
         jLabel3.setText("Status");
 
         jButton1.setText("LOGIN");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Need an account?");
+
+        jButton2.setText("REGISTER");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(90, 90, 90)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(92, 92, 92))
+                .addGap(85, 85, 85)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(jPasswordField1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE)
+                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(97, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(38, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -86,17 +106,36 @@ public class LoginForm extends javax.swing.JPanel {
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGap(58, 58, 58))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addContainerGap(56, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String user = "";
+        String pass = "";
+        String status = "";
+        // @TODO: Verify login fields
+        Command cmd = new Command("Login", user, pass, status);
+        // @TODO: Send login command to ServerHandler
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // @TODO: Send any entered data to Registration form and open
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
---
+++ Grimace/src/grimace/client/ProgramSettings.java
@@ -0,0 +1,62 @@
+package grimace.client;
+
+import java.util.prefs.*;
+
+public class ProgramSettings {
+    Preferences prefs;
+
+    public ProgramSettings() throws BackingStoreException {
+        loadSettings(Preferences.systemRoot().nodeExists("/Wernicke/ProgramSettings"));
+    }
+
+    public void loadSettings(Boolean exists) {
+        prefs = Preferences.systemRoot().node("/Wernicke/ProgramSettings");
+        if (!exists) {
+            setServerAddress("127.0.0.1");
+            setServerPort(4422);
+            setShowInTray(true);
+            setShowEmoticons(true);
+            setRecordChatLogs(false);
+        }
+    }
+
+    public void setServerAddress(String address) {
+        prefs.put("ServerAddress", address);
+    }
+
+    public String getServerAddress() {
+        return prefs.get("ServerAddress", null);
+    }
+
+    public void setServerPort(int port) {
+        prefs.putInt("ServerPort", port);
+    }
+
+    public int getServerPort() {
+        return prefs.getInt("ServerPort", 0);
+    }
+
+    public void setShowInTray(Boolean enabled) {
+        prefs.putBoolean("ShowInTray", enabled);
+    }
+
+    public Boolean getShowInTray() {
+        return prefs.getBoolean("ShowInTray", true);
+    }
+
+    public void setShowEmoticons(Boolean enabled) {
+        prefs.putBoolean("ShowEmoticons", enabled);
+    }
+
+    public Boolean getShowEmoticons() {
+        return prefs.getBoolean("ShowEmoticons", true);
+    }
+
+    public void setRecordChatLogs(Boolean enabled) {
+        prefs.putBoolean("RecordChatLogs", enabled);
+    }
+
+    public Boolean getRecordChatLogs() {
+        return prefs.getBoolean("RecordChatLogs", false);
+    }
+}
