91743d58080f0423649e1aa95e340c23c2aee256
jjcole <jjcole@708ad52c-d002-11de-97bf-6350044336de>
2009-11-30 05:21:00 +0000

    Added license header to files.

--- trunk/Grimace/src/grimace/client/RegistrationForm.java
+++ trunk/Grimace/src/grimace/client/RegistrationForm.java
@@ -1,20 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
+/**
  * RegistrationForm.java
  *
- * Created on 29-Nov-2009, 12:29:14 PM
+ * @author Justin Cole
+ *
+ * Copyright (C) 2009 Justin Cole, Aaron Jankun, David Marczak, Vineet Sharma,
+ *        and Aaron Toth
+ *
+ * This file is part of Wernicke.
+ *
+ * Wernicke is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package grimace.client;
 
-/**
- *
- * @author Huxley
- */
 public class RegistrationForm extends javax.swing.JPanel {
 
     /** Creates new form RegistrationForm */
