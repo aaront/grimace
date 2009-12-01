0ecb98f0a466c92ccdf524c1e13c8a21a1677bd8
vineet.sharma10 <vineet.sharma10@708ad52c-d002-11de-97bf-6350044336de>
2009-12-01 02:16:48 +0000

    Added/completed functions in DataHandler and added documentation for all methods in DataHandler.

--- Grimace/src/grimace/server/DataHandler.java
+++ Grimace/src/grimace/server/DataHandler.java
@@ -25,12 +25,14 @@
 package grimace.server;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.security.MessageDigest;
 import grimace.client.Account;
 import grimace.client.ContactList;
+import grimace.client.Contact;
 
 /**
- *
+ * DataHandler provides methods for interfacing with Wernicke's server database.
  * @author Vineet Sharma
  */
 public class DataHandler {
@@ -46,6 +48,11 @@ public class DataHandler {
         connection = DriverManager.getConnection(CONNECTION_URL);
 	}
 
+    /**
+     * Creates all necessary tables if the do not exist.
+     *
+     * @throws java.sql.SQLException
+     */
     public void initDatabase() throws SQLException {
         DataHandler.createTable("Accounts", false,
                                 "userName varchar(30)",
@@ -59,10 +66,21 @@ public class DataHandler {
                                 "connection int");
         DataHandler.createTable("Contacts", false,
                                 "userName varchar(30)",
-                                "contactName varchar(30)",
-                                "contactNick varchar(100)");
+                                "contactName varchar(30)");
     }
 
+    /**
+     * Creates a table with the given table name and columns.
+     *
+     * @param tableName The name of the table to create
+     * @param replace   Whether or not to overwrite an existing table with the
+     *                  same name.
+     * @param cols      The columns of the table, each followed by its type,
+     *                  separated by a space.
+     *                  For example, "wernickeCol varchar(100)" represents a
+     *                  column named wernickeCol of type varchar(100).
+     * @throws java.sql.SQLException
+     */
 	private static void createTable(String tableName,
                                     boolean replace,
                                     String... cols) throws SQLException {
@@ -78,7 +96,16 @@ public class DataHandler {
         statement.close();
 	}
 
-    public static void createAccount(String userName,
+    /**
+     * Creates an Account witht the given userName and password.
+     *
+     * @param userName  The userName for the new account.
+     * @param passWord  The password for the new account, which will be stored
+     *                  as a hexadecimal representation of the SHA-1 hash of the
+     *                  string provided.
+     * @throws java.sql.SQLException
+     */
+    public static synchronized void createAccount(String userName,
                                     String passWord) throws SQLException {
         Statement statement = connection.createStatement();
         String sql = "INSERT INTO Accounts (userName, passWord) VALUES(\'"
@@ -88,7 +115,13 @@ public class DataHandler {
         statement.close();
     }
 
-    private static String getPasswordHash(String passWord) {
+    /**
+     * Returns a hash of the given password string using the SHA-1 algorithm.
+     *
+     * @param passWord  The string to hash.
+     * @return  A string of hex digits representing the hashed value.
+     */
+    private static synchronized String getPasswordHash(String passWord) {
         MessageDigest md;
         try {
             md = MessageDigest.getInstance("SHA-1");
@@ -112,29 +145,155 @@ public class DataHandler {
         return hash.toString();
     }
 
+    /**
+     * Updates the database with the information in the given account.
+     *
+     * @param acc   The account to save.
+     */
 	public static void saveAccount(Account acc) {
         
 	}
 
-	public static Account loadAccount(String username) throws SQLException {
+    /**
+     * Returns an Account with the given userName and the settings associated
+     * with that userName.
+     *
+     * @param userName  The userName of the Account to load.
+     * @return  An Account containing the given userName and settings associated
+     *          with that userName.
+     * @throws java.sql.SQLException
+     */
+	public static synchronized Account loadAccount(String userName) throws SQLException {
         Statement statement = connection.createStatement();
-        String sql = "SELECT * FROM Accounts WHERE userName=\'" + username +"\'";
+        String sql = "SELECT * FROM Accounts WHERE userName=\'" + userName +"\'";
         ResultSet result = statement.executeQuery(sql);
         return null;
 	}
 
-	public static void deleteAccount(String username) throws SQLException {
+    /**
+     * Returns the display name associated with the account with the given
+     * userName.
+     *
+     * @param userName  The userName of the account whose displayName is
+     *                  required.
+     * @return  The displayName of the Account with the given userName, or an
+     *          empty string if that Account does not exist.
+     */
+    public static String getDisplayName(String userName) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT displayName FROM Accounts WHERE userName=\'" + userName +"\'";
+            ResultSet result = statement.executeQuery(sql);
+            statement.close();
+            if (!result.next()) { return ""; }
+            String dname = result.getNString("displayName");
+            result.close();
+            return dname;
+        }
+        catch (Exception e) { return ""; }
+    }
+
+    /**
+     * Deletes an account with the given userName.
+     *
+     * @param userName  The userName of the account to delete.
+     * @throws java.sql.SQLException
+     */
+	public static synchronized void deleteAccount(String userName) throws SQLException {
         Statement statement = connection.createStatement();
         String sql = "DELETE FROM Accounts WHERE userName="
-                        + username + " LIMIT 1";
+                        + userName + " LIMIT 1";
         statement.executeUpdate(sql);
 	}
 
-	public static void saveContactList(ContactList listt) {
-        
+    /**
+     * Returns whether or not an account with the given userName exists.
+     *
+     * @param userName  The userName to check.
+     * @return  True if an Account with the given userName exists, and false
+     *          otherwise.
+     */
+    public static synchronized boolean accountExists(String userName) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM Accounts WHERE userName=\'"
+                        + userName +"\'";
+            ResultSet result = statement.executeQuery(sql);
+            if (!result.next()) { return false; }
+        }
+        catch (Exception e) { return false; }
+        return true;
+    }
+
+    /**
+     * Returns whether or not an account has a given contact in its contactList.
+     *
+     * @param userName  The userName to check.
+     * @param contactName The contact to check for.
+     * @return  True if an Contact with the given contactName exists in the
+     *          ContactList of the Account with the given userName and false
+     *          otherwise.
+     */
+    public static synchronized boolean contactExists(String userName,
+                                                        String contactName) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM Contacts WHERE userName=\'"
+                            + userName +"\' AND contactName=\'"
+                            + contactName +"\'";
+            ResultSet result = statement.executeQuery(sql);
+            if (!result.next()) { return false; }
+        }
+        catch (Exception e) { return false; }
+        return true;
+    }
+
+    /**
+     * Saves a ContactList by inserting any Contacts that do not already exist
+     * and deleting any Contacts that are not found in the given list.
+     *
+     * @param userName The userName of the Account that holds the list.
+     * @param list  The ContactList to save.
+     */
+    public static synchronized void saveContactList(String userName,
+                                                    ContactList list) {
+        if (!accountExists(userName)) { return; }
+        ArrayList<Contact> contacts = list.getList();
+        for (Contact c : contacts) {
+            if (!contactExists(userName, c.getUserName())) {
+                try {
+                    Statement statement = connection.createStatement();
+                    String sql = "INSERT INTO Contacts VALUES(\'"
+                                + userName +"\', \'"
+                                + c.getUserName() + "\')";
+                    statement.execute(sql);
+                }
+                catch (Exception e) {}
+            }
+        }
+
 	}
 
-	public static ContactList loadContactList(String username) {
-        return null;
+    /**
+     * Returns the ContactList for an account.
+     *
+     * @param userName  The userName of the account.
+     * @return  A ContactList with all contacts associated with the given
+     *          userName.
+     */
+	public static synchronized ContactList loadContactList(String userName) {
+        try {
+            ContactList cList = new ContactList();
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM Accounts WHERE userName=\'"
+                        + userName +"\'";
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                Contact c = new Contact(result.getString("contactName"),
+                                        result.getNString(""));
+            }
+            return cList;
+        }
+        catch (Exception e) { return null; }
 	}
 }
